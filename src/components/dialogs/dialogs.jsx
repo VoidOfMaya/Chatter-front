164eece5fa224dc62ad1e2afacada1da3f8af24c
Maya <MayaOfTheVoid@gmail.com>
2026-05-25 10:28:57 +0300

    implements and connects register dialog to server

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -44,52 +44,97 @@ const LoginDialog = ({referance, close})=>{
 
 const SignupDialog = ({referance, close}) =>{
     const [data, setData]= useState({
-        name: null,
-        email: null,
-        password: null,
-        confirmPassword:null
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword:''
     })
     return(
         <dialog ref={referance}>
             <div style={{gridArea:'title', justifySelf: 'center'}}>Signup</div>
 
-            <form   style={{gridArea:'form'}} 
+            <form   action='http://localhost:3000/auth/register' method='POST'
+                    style={{gridArea:'form'}} 
                     className={style.signupForm}
-                    onSubmit={(e)=>{
+                    onSubmit={async (e)=>{
                         e.preventDefault();
-                        referance.current.close()
-                        close()
+                        //creates the form body to submit to server
+                        const registerData = new URLSearchParams();
+                        registerData.append('email', data.email);
+                        registerData.append('name', data.name);
+                        registerData.append('password', data.password);
+                        registerData.append('confirmPassword', data.confirmPassword);
+                        //submits 
+                        try{
+                        fetch('http://localhost:3000/auth/register',{
+                                method: 'POST',
+                                headers:{
+                                    'Content-Type': 'application/x-www-form-urlencoded',
+                                },
+                                body:registerData,
+                            })
+                            .then((response)=>{
+                                if(!response.ok) throw new Error(response.msg);
+                                return response.json()
+                            })
+                            .then((data) => {
+                                console.log('Success! Data received:', data);
+                            })
+                            .catch((error) => {
+                                console.error('Fetch failed:', error);
+                            });
+
+                            referance.current.close()
+                            close()                            
+                        }catch(err){
+                            console.error(err.message)
+                        }
+ 
                     }}>
-                <label htmlFor='Email'>Email :</label>
-                <input  type='email' id='Email' name='Email' placeholder="email" required></input>
+                <label htmlFor='email'>Email :</label>
+                <input  type='email' id='email' name='email' placeholder="email" required 
+                    value={data.email}
+                onChange={(e)=>
+                    setData((prev)=>({
+                        ...prev, email: e.target.value,
+                    }))
+                }
+                ></input>
 
-                <label htmlFor='firstName'>First name :</label>
-                <input  type='text' id='firstName' name='firstName' placeholder="First name" min='3'max='12' required>
-                </input>
-
-                <label htmlFor='lastName'>Last name :</label>
-                <input  type='text' id='lastName' name='lastName' placeholder="Last name" min='3' max='12' required>
-                </input>
+                <label htmlFor='name'>First name :</label>
+                <input  type='' id='name' name='name' placeholder="name" minLength='3'maxLength='12' required
+                value={data.name}
+                onChange={(e)=>
+                    setData((prev)=>({
+                        ...prev, name: e.target.value,
+                    }))
+                }
+                ></input>
 
                 <label htmlFor='password'>Password :</label>
-                <input  type='password' id='password' name='password' placeholder="password" min='8' required
+                <input  type='password' id='password' name='password' placeholder="password" minLength='8' required
+
+                        value={data.password}
+                        onChange={(e)=>
+                            setData((prev)=>({
+                                ...prev, 
+                                password: e.target.value,
+                            }))
+                        }
                         className={ data.password !== data.confirmPassword? style.invalidField : style.validField}
                 ></input>
 
                 <label htmlFor='confirmPass'>Confirm password :</label>
-                <input  type='password' 
-                        id='confirmPass'
-                        name='confirmPass'
-                        placeholder="Confirm password"
-                        min='8'
-                        required
+                <input  type='password' id='confirmPass'name='confirmPass'
+                        placeholder="Confirm password"minLength='8'required
+
+                        value={data.confirmPassword}
                         onChange={(e)=>setData(prev=>({...prev, confirmPassword: e.target.value}))}
                         className={ data.password !== data.confirmPassword? style.invalidField : style.validField}
                 ></input>
                 
                 <button type='button'
                     onClick={(e)=>{
-                        e.preventDefault()
                         referance.current.close();
                         close()
                     }}
